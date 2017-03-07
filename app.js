906e3b1d584b3d86efe77a86240cd7869c772ca6
Hannah Kim <hankim53@gmail.com>
2017-03-07 12:28:12 -0500

    rendering is working

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const express = require( 'express' );
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
+app.set('view engine', 'html');
 
-app.get('/', function (req, res) {
+ app.use(volleyball);
+//to make app.use always run, we place it in top
+// app.use(function (req, res, next) {
+//     console.log(req);
+//     next();
+// })
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views', {noCache: true, autoescape: true,
+    express: app});
+app.get('/', function(req, res) {
+    res.render('index', locals);
+});
+
+app.use('/special/', function (req, res, next) {
   res.send('Hello World!')
 })
 
+app.get('/', function (req, res, next) {
+  res.send('Hello World!')
+  console.log("goodbye world");
+  next();
+})
+
+app.get('/news', function (req, res) {
+  res.send('Get news')
+})
+
+//just establish connection once
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
