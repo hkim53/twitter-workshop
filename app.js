e8dd9962d508471631230f36f30c884c747f0d47
Hannah Kim <hankim53@gmail.com>
2017-03-07 10:37:10 -0500

    committing first

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
